8f051ed99ef5cc0e2b545cb7d49cdeb016a89d70
Taylor Spooner <spooner.taylor@gmail.com>
2017-12-08 11:32:39 -0500

    Begin analysis on subjects

---
+++ Taylor/subjects.R
@@ -0,0 +1,55 @@
+# Associations Between Subjects
+library(data.table)
+# Read in the subject information:
+subs = fread("data/course_institution_info_english.csv")
+subs$course_id = as.numeric(subs$course_id)
+subs$term_id = as.numeric(subs$term_id)
+subs$institution_id = as.numeric(subs$institution_id)
+# Aggregate the names
+subs[, course_name := NULL,]
+subs_unq = subs[,head(.SD, 1), by = .(institution_id, term_id, course_subject_EN)]
+# Read in the grade counts
+grades = fread("data/grade_counts.csv")
+grades$course_id = as.numeric(grades$course_id)
+grades$term_id = as.numeric(grades$term_id)
+grades$institution_id = as.numeric(grades$institution_id)
+# Merge the two together
+data = merge(grades, subs, by = c("institution_id","term_id", "course_id"), all.x=TRUE)
+
+# We are now going to only get subjects that are in all three schools.
+schools = unique(data$institution_id)
+un_courses = list()
+for(s in schools) {
+  un_courses[[as.character(s)]] = unique(subs[institution_id == s, course_subject_EN,]) 
+}
+courses = Reduce(intersect, un_courses)
+# We are left with 12 courses. That will create 66 pairwise tests for independence.
+
+# Subset data to only included the courses in the list.
+data_sub = data[course_subject_EN %in% courses,,]
+
+######################################
+# Run Tests For Independence
+## But this is ordinal data.
+######################################
+
+# Aggregate down to just the term and course, forget school.
+data_agg = data_sub[, count := sum(as.numeric(c)), by=.(course_subject_EN, grade_value)]
+data_agg = data_agg[, head(.SD,1), by=.(course_subject_EN, grade_value,count)]
+data_agg = data_agg[, .(course_subject_EN, grade_value, count),]
+data_agg = data_agg[, ':='(grade_value = as.numeric(grade_value),
+                           count = as.numeric(count))]
+
+# Fill in the missing grades
+full = expand.grid(unique(data_agg$course_subject_EN), c(1.00, 2.00, 3.00, 4.00, 5.00))
+full = data.table(course_subject_EN = full$Var1, grade_value = full$Var2)
+
+data_agg = merge(full, data_agg, by = c("course_subject_EN", "grade_value"), all.x=TRUE)
+data_agg[is.na(count), count := 0,]
+
+# Now we can begin our tests for independence
+
+bio = data_agg[course_subject_EN =="Biology",]
+chem = data_agg[course_subject_EN == "Chemisty",]
+
+
