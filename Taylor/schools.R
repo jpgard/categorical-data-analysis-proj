9ab76d3dc072ab852aa83aa59f445e246328d3b5
Taylor Spooner <spooner.taylor@gmail.com>
2017-12-08 17:52:02 -0500

    Mixed effects model in schools, update notes

--- Taylor/schools.R
+++ Taylor/schools.R
@@ -27,15 +27,6 @@ setkey(data_fin, institution_id, course_subject_EN)
 # Now we have full grade records for all common courses for each school.
 # Test for homogeniety
 
-# For each subject
-temp = data_fin[course_subject_EN == "Biology",]
-temp2 = dcast(temp, grade_value ~ institution_id, value.var = "count")
-chisq.test(temp2)
-
-ftable(matrix(temp2[]))
-temp3 = t(as.matrix(temp2, nrow = 5, 
-                    dimnames = list(c("Biology"),c("1","2","3","4","5"), c("519","523", "524"))))
-
 ##############################
 # Disregard the ordinal nature of the grades
 # Just do test for independence
@@ -44,9 +35,12 @@ n = length(courses)
 course_ind = data.frame(Subject = rep(NA,n), pval = rep(NA,n), sig = rep(NA,n))
 options(warn = -1)
 for(i in 1:n) {
-  temp = data_fin[course_subject_EN == courses[i] & grade_value > 2,]
+  temp = data_fin[course_subject_EN == courses[i],]
   temp2 = dcast(temp, grade_value ~ institution_id, value.var = "count")
   print(temp2)
+  # Another way of showing the data
+  # temp3 = t(as.matrix(temp2, nrow = 5, 
+  #                     dimnames = list(c("Biology"),c("1","2","3","4","5"), c("519","523", "524"))))
   test = chisq.test(temp2)
   course_ind$Subject[i] = courses[i]
   course_ind$pval[i] = test$p.value
@@ -59,8 +53,25 @@ for(i in 1:n) {
 }
 options(warn = 0)
 
-# Seems that most of the 0's are at the 1 and 2 grade level.
-# Even taking those out and doing a test for independence between values 3, 4 and 5 
-## we get that all the p-values are 0.
-## There is no evidence that the schools have different grade distributions.
 
+##############################  
+# School modeling
+##############################
+library(lme4)
+# Run mixed effect model with school as random effect
+expand_df = data.table(institution_id = rep(data_fin$institution_id, floor(data_fin$count*1/4)),
+                       grade_value = rep(data_fin$grade_value, floor(data_fin$count*1/4)),
+                       course_subject_EN = rep(data_fin$course_subject_EN, floor(data_fin$count*1/4)))
+summary(factor(expand_df$grade_value))
+expand_df[, pass := ifelse(grade_value <= 3, 1, 0),]
+
+school.fit = glmer(pass ~ factor(course_subject_EN) + (1|institution_id), data = expand_df, family =binomial)
+
+# 
+expand_df = data.table(institution_id = rep(data_fin$institution_id, floor(data_fin$count*.1)),
+                       grade_value = rep(data_fin$grade_value, floor(data_fin$count*.1)),
+                       course_subject_EN = rep(data_fin$course_subject_EN, floor(data_fin$count*.1)))
+summary(factor(expand_df$grade_value))
+expand_df[, pass := ifelse(grade_value <= 3, 1, 0),]
+
+school.fit2 = glmer(pass ~ course_subject_EN + (course_subject_EN|institution_id), data = expand_df, family =binomial)
--- Taylor/subjects.R
+++ Taylor/subjects.R
@@ -1,27 +1,19 @@
 # Associations Between Subjects
+library(data.table)
 source("Taylor/data_prep.R")
-######################################
-# Run Tests For Independence
-## But this is ordinal data.
-######################################
-# Aggregate down to just the term and course, forget school.
-data_agg = data_sub[, count := sum(as.numeric(c)), by=.(course_subject_EN, grade_value)]
-data_agg = data_agg[, head(.SD,1), by=.(course_subject_EN, grade_value,count)]
-data_agg = data_agg[, .(course_subject_EN, grade_value, count),]
-data_agg = data_agg[, ':='(grade_value = as.numeric(grade_value),
-                           count = as.numeric(count))]
 
-# Fill in the missing grades
-full = expand.grid(unique(data_agg$course_subject_EN), c(1.00, 2.00, 3.00, 4.00, 5.00))
-full = data.table(course_subject_EN = full$Var1, grade_value = full$Var2)
+# Read in student level data.
+students = fread("data/student_grades_total.csv")
+students[, ':='(student_id = as.numeric(student_id),
+                institution_id = as.numeric(institution_id),
+                term_id = as.numeric(term_id),
+                course_id = as.numeric(course_id),
+                grade_value = as.numeric(grade_value)),]
+# Merge in english subjects
+stud = merge(students, subs, by = c("institution_id","term_id","course_id"), all.x=TRUE)
+stud[, ':='(course_name = NULL,
+                course_subject = NULL),]
 
-data_agg = merge(full, data_agg, by = c("course_subject_EN", "grade_value"), all.x=TRUE)
-data_agg[is.na(count), count := 0,]
-
-
-# Now we can begin our tests for independence
-
-bio = data_agg[course_subject_EN =="Biology",]
-chem = data_agg[course_subject_EN == "Chemisty",]
-
+### Try with two subjects
+temp = students[course_subject_EN %in% c("Biology", "Chemistry"),,]
 
